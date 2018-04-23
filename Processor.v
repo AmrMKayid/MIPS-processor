fe027d65f74f48f16c29fcd494253d184f23c4af
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-23 16:31:07 +0200

    Fix problem with the Instruction Mux

--- Processor.v
+++ Processor.v
@@ -9,15 +9,15 @@ module Processor(clk);
 
 	input clk;
 
-	wire[31: 0] pc, instruction, ReadData1, ReadData2, ALUOut, ALUCtrlOut, SEOut, PCin0, PCin1, shl, ALUroute, MemRoute, PCRoute, DataMemoryOut;
+	wire[31: 0] pc, instruction, ReadData1, ReadData2, ALUOut, SEOut, PCin0, PCin1, shl, ALUroute, MemRoute, PCRoute, DataMemoryOut;
 	wire[3:0] ALUCtrlOut;
-	wire[15:0] RdRoute;
+	wire[4:0] RdRoute;
 	wire RegDst, RegWrite, ALUSrc, Branch, MemRead, MemWrite, MemtoReg, ALUZero;
 	wire [1:0] ALUop;
 
 	Adder PCadder0(PCin0, pc, 4);
 	Adder PCadder1(PCin1, PCin0, shl);
-	shiftLeft2(shl, input1);
+	shiftLeft2 SHL2(shl, SEOut);
 	Mux2way32 PCMux(PCRoute, PCin0, PCin1, PCsrc);
 
 	ProgramCounter PC(pc, PCRoute, clk);
@@ -26,17 +26,17 @@ module Processor(clk);
 
 	RegisterFile RF(ReadData1, ReadData2, instruction[25:21], instruction[20:16], RdRoute, MemRoute, RegWrite, clk);
 
-	SignExtend(SEOut, instruction[15:0]);
+	SignExtend SE(SEOut, instruction[15:0]);
 
 	ALUControl ALUCtrl(ALUCtrlOut, ALUop, instruction[5:0]);
-	ALU ALU(ALUOut, ALUZero ALUCtrlOut, ReadData1, ALUroute);   
+	ALU ALU(ALUOut, ALUZero, ALUCtrlOut, ReadData1, ALUroute);   
 	
 	DataMemory DM(DataMemoryOut, ReadData2, ALUOut, MemRead, MemWrite, clk);
 
-	ControlUnit(RegDst, RegWrite, ALUSrc, Branch, MemRead, MemWrite, MemtoReg, ALUop, instruction[31:26]);
+	ControlUnit CU(RegDst, RegWrite, ALUSrc, Branch, MemRead, MemWrite, MemtoReg, ALUop, instruction[31:26]);
 	
 	and PCSrc(PCsrc, ALUZero, Branch);
-	Mux2way16 InstructionMux(RdRoute, instruction[20:16], instruction[15:11], RegDst);
+	Mux2way5 InstructionMux(RdRoute, instruction[20:16], instruction[15:11], RegDst);
 
 	Mux2way32 RegMux(ALUroute, ReadData2, SEOut, ALUSrc);
 
--- tests/Mux2way5_tb.v
+++ tests/Mux2way5_tb.v
@@ -3,20 +3,20 @@
   * Makes sure the Mux behaves as expected
   *
   */
-module Mux2way16_tb();
+module Mux2way5_tb();
 
-	reg[31:0] in;
+	reg[9:0] in;
 	reg sel; 
-	wire[15:0] out;
+	wire[4:0] out;
 	initial begin
 	in = 0;
 	sel = 0;
 	end
-Mux2way16 m(out[15:0], in[15:0], in[31:16], sel);
+Mux2way16 m(out[9:0], in[9:0], in[9:5], sel);
 
 
 	initial begin
-		$monitor("time=%t",$time ,"  a=%d", in[15:0],"  b=%d", in[31:16],"  sel=%b", sel,  "", "  out:%d", out);
+		$monitor("time=%t",$time ,"  a=%d", in[5:0],"  b=%d", in[9:5],"  sel=%b", sel,  "", "  out:%d", out);
 		repeat(16) begin
 			repeat(2) begin
 				#5 sel = sel + 1;
@@ -27,7 +27,7 @@ Mux2way16 m(out[15:0], in[15:0], in[31:16], sel);
 			repeat(2) begin
 				#5 sel = sel + 1;
 			end
-		in[31:16] = in[31:16] + 1;
+		in[9:5] = in[9:5] + 1;
 		end
 		#200 $finish;
 	end
